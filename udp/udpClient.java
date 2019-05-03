14cdf63140f8debc3dd95f39d47ef87b0ca98d31
VineethKumar7 <k.vineeth26@gmail.com>
2019-05-03 06:35:50 +0530

    UDP changed

--- udp/udpClient.java
+++ udp/udpClient.java
@@ -5,8 +5,7 @@ class udpClient
 {
    public static void main(String args[]) throws Exception
    {
-      BufferedReader inFromUser =
-         new BufferedReader(new InputStreamReader(System.in));
+      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
       DatagramSocket clientSocket = new DatagramSocket();
       InetAddress IPAddress = InetAddress.getByName("localhost");
       byte[] sendData = new byte[1024];
@@ -14,9 +13,11 @@ class udpClient
       String sentence = inFromUser.readLine();
       sendData = sentence.getBytes();
       DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-      clientSocket.send(sendPacket);
+	  clientSocket.send(sendPacket);
+	  
       DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-      clientSocket.receive(receivePacket);
+	  clientSocket.receive(receivePacket);
+	  
       String modifiedSentence = new String(receivePacket.getData());
       System.out.println("FROM SERVER:" + modifiedSentence);
       clientSocket.close();
--- udp/udpServer.java
+++ udp/udpServer.java
@@ -6,7 +6,7 @@ class udpServer
    public static void main(String args[]) throws Exception
       {
             System.out.println("UDP server");
-            System.out.println("Server waiting for connetion request");
+            System.out.println("Server is always connected . \n Enter message ");
             DatagramSocket serverSocket = new DatagramSocket(9876);
             byte[] receiveData = new byte[1024];
             byte[] sendData = new byte[1024];
